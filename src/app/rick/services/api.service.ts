d06a07d905c591a779939c5e4e60d0c7d08cbeb2
Luis Rojas <lumrojasmu@unal.edu.co>
2023-06-15 12:28:26 -0500

    Trabajo final

---
+++ src/app/rick/services/api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import{HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl ='https://rickandmortyapi.com/api';
+  options ={
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+
+
+    }),
+
+  }
+
+  constructor(private http: HttpClient) { }
+  
+  getCharacters(){
+    return this.http.get(this.baseUrl+'/character', this.options);
+  }
+}
